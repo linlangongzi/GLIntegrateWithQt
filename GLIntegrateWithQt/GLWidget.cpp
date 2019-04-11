c4cfb723287b04d7261575d16eced5dd99db3a13
linlancloud <353258146@qq.com>
2019-04-11 17:28:45 +0800

    添加错误校验代码

--- GLIntegrateWithQt/GLWidget.cpp
+++ GLIntegrateWithQt/GLWidget.cpp
@@ -1,19 +1,63 @@
 #include "GLWidget.h"
+#include <iostream>
+#include <QDebug>
 
 GLWidget::GLWidget(QOpenGLWidget *parent)
 	: QOpenGLWidget(parent)
 {
 	ui.setupUi(this);
+	//m_surface = new QSurface();
+	//m_context = new QOpenGLContext();
+	//m_context->makeCurrent();
 }
 
 GLWidget::~GLWidget()
 {
 }
 
+void GLWidget::initializeShaderProgram()
+{
+	m_program.create();
+	if (checkProgramStatus( m_program.programId()))
+	{
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+	}
+
+}
+bool GLWidget::checkStatus(GLuint objectID, PFNGLGETSHADERIVPROC objectPropertyAccesserFunc, PFNGLGETSHADERINFOLOGPROC getInfoLogFunc, GLenum statusType)
+{
+	GLint status;
+	objectPropertyAccesserFunc(objectID, statusType, &status);
+	if (status != GL_TRUE)
+	{
+		GLint infoLength;
+		objectPropertyAccesserFunc(objectID, GL_INFO_LOG_LENGTH, &infoLength);
+		GLchar* logBuffer = new GLchar(infoLength);
+		
+		GLsizei bufferSize;
+		getInfoLogFunc(objectID, infoLength, &bufferSize, logBuffer);
+		std::cout << logBuffer;
+		delete logBuffer;
+		return false;
+	}
+	return true;
+}
+
+bool GLWidget::checkShaderStatus(GLuint shaderID)
+{
+	return checkStatus(shaderID, /*(PFNGLGETSHADERIVPROC)wglGetProcAddress("glGetShaderiv")*/
+		(PFNGLGETSHADERIVPROC)context()->getProcAddress("glGetShaderiv"), (PFNGLGETSHADERINFOLOGPROC)wglGetProcAddress("glGetShaderInfoLog"), GL_COMPILE_STATUS);
+}
+bool GLWidget::checkProgramStatus(GLuint programID)
+{
+	return checkStatus(programID, (PFNGLGETSHADERIVPROC) context()->getProcAddress("glGetProgramiv"), (PFNGLGETSHADERINFOLOGPROC) wglGetProcAddress("glGetProgramInfoLog"), GL_COMPILE_STATUS);
+}
 void GLWidget::initializeGL()
 {
 	initializeOpenGLFunctions();
 	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+	initializeShaderProgram();
 }
 
 void GLWidget::paintGL()
--- GLIntegrateWithQt/GLWidget.h
+++ GLIntegrateWithQt/GLWidget.h
@@ -6,7 +6,7 @@
 #include <QOpenGLTexture>
 #include <QMatrix4x4>
 #include <QOpenGLBuffer>
-
+#include <qopenglext.h>
 #include "ui_GLWidget.h"
 
 class GLWidget : public QOpenGLWidget, protected QOpenGLFunctions
@@ -21,12 +21,22 @@ protected:
 	void mousePressEvent(QMouseEvent *e) override;
 	void mouseReleaseEvent(QMouseEvent *e) override;
 
+
 	void initializeGL() override;
 	void paintGL() override;
 	void resizeGL(int w, int h) override;
 
+	bool checkShaderStatus(GLuint shaderID);
+	bool checkProgramStatus(GLuint programID);
+
+private:
+	void initializeShaderProgram();
+	bool checkStatus(GLuint objectID, PFNGLGETSHADERIVPROC objectPropertyAccesserFunc, PFNGLGETSHADERINFOLOGPROC getInfoLogFunc, GLenum statusType);
 private:
 	QOpenGLShaderProgram m_program;
+	//QOpenGLContext *m_context;
+	//QOpenGLFormat * m_format;
+	//QSurface *m_surface;
 
 private:
 	Ui::GLWidget ui;
